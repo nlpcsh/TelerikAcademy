f0df4274d2270938ad3daf5511390e2351bf7f50
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-02-08 10:05:32 +0200

    Homework CSharp II - Arrays (01) - Tasks 1 - 2

---
+++ Homeworks/CSharp_II/hw_csh_II_01_Arrays/Task02_Compare Arrays/CompareArrays.cs
@@ -0,0 +1,19 @@
+﻿// Task 2 - Write a program that reads two integer arrays from the console and
+//   compares them element by element.
+
+using System;
+using System.Linq;
+
+class CompareArrays
+{
+    static void Main()
+    {
+        Console.WriteLine(" Enter a line of numbers for the first array (1 2 4 6 8...): ");
+        int[] arrayOne = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+        Console.WriteLine(" Enter a line of numbers for the second array (11 22 41 65 8...): ");
+        int[] arrayTwo = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+
+    }
+}
---
+++ Homeworks/CSharp_II/hw_csh_II_01_Arrays/hw_csh_II_01_Arrays/AllocateArray.cs
@@ -0,0 +1,20 @@
+﻿// Task 1 - Write a program that allocates array of 20 integers and initializes
+//     each element by its index multiplied by 5.
+//   Print the obtained array on the console.
+
+using System;
+
+class AllocateArray
+{
+    static void Main()
+    {
+        int[] twentyElements = new int[20];
+
+        for (int i = 0; i < twentyElements.Length; i++)
+        {
+            twentyElements[i] = i * 5;
+            Console.WriteLine(" index {0}  -  value  {1}", i, twentyElements[i]);
+        }
+    }
+}
+
