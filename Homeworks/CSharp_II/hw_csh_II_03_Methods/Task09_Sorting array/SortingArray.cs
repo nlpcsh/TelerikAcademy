e26528e15e686ea1ed191a2e66afaa1405473399
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-02-21 19:43:43 +0200

    Homework CSharp II - Methods (03) - Tasks 10 - 14

--- Homeworks/CSharp_II/hw_csh_II_03_Methods/Task09_Sorting array/SortingArray.cs
+++ Homeworks/CSharp_II/hw_csh_II_03_Methods/Task09_Sorting array/SortingArray.cs
@@ -3,6 +3,7 @@
 //   Using it write another method that sorts an array in ascending / descending order.
 
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,76 @@ class SortingArray
 {
     static void Main()
     {
-        Console.Write(" Enter int numbers to form an array (1 3 5...): ");
+        
+        StreamReader reader = new StreamReader("..\\..\\inputData.txt");
+        Console.SetIn(reader);
+
+        Console.WriteLine(" Enter int numbers to form an array (1 3 5...): ");
         int[] intNumbers = Console.ReadLine()
                                   .Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries)
                                   .Select(int.Parse)
                                   .ToArray();
 
-        Console.Write(" Enter : ");
-        string strNumber1 = Console.ReadLine();
+        // Print the initial array:
+        Console.WriteLine(string.Join(",", intNumbers));
 
+        Console.WriteLine(" Enter an index to search the array from it: ");
+        int strIndex = int.Parse(Console.ReadLine());
+
+        // Print the input index
+        Console.WriteLine(strIndex);
+
+        Console.WriteLine(" Max element is: {0}", SearchForMaxFromIndex(intNumbers, strIndex));
+
+        intNumbers = SortTheArary(intNumbers);
+
+        Console.WriteLine(" Sorted array is: ");
+        Console.WriteLine(string.Join(",", intNumbers));
+
+    }
+
+    static int SearchForMaxFromIndex(int[] intArray, int index)
+    {
+        int maxElement = intArray[index];
+
+        if (index < 0 || index > intArray.Length-1)
+        {
+            Console.WriteLine("Index is out of the array range! ");
+
+            Environment.Exit(0);
+            
+        }
+
+        for (int i = index; i < intArray.Length; i++)
+        {
+            if (maxElement < intArray[i])
+            {
+                maxElement = intArray[i];
+            }
+        }
+
+        return maxElement;
+    }
+
+    static int[] SortTheArary(int[] arrayInt)
+    {
+
+        for (int i = 0; i < arrayInt.Length; i++)
+        {
+            int maxValue = SearchForMaxFromIndex(arrayInt, i);
+            int indexMaxValue = Array.LastIndexOf(arrayInt, maxValue);
+
+            if (indexMaxValue != i)
+            {
+                int exchangeValue = arrayInt[i];
+                arrayInt[i] = arrayInt[indexMaxValue];
+                arrayInt[indexMaxValue] = exchangeValue;
+            }
+        }
+
+        Array.Reverse(arrayInt);
+
+        return arrayInt;
     }
 }
 
---
+++ Homeworks/CSharp_II/hw_csh_II_03_Methods/Task10_N Factorial/NFactorial.cs
@@ -0,0 +1,39 @@
+﻿// Task 10 - Write a program to calculate n! for each n in the range [1..100].
+
+using System  ;
+using System.Numerics;
+
+class NFactorial
+{
+    static void Main()
+    {
+        Console.Write(" Enter a number N to calculate N! : ");
+        sbyte N = sbyte.Parse(Console.ReadLine());
+
+        CalculateNFact(N);
+    }
+    static void CalculateNFact(sbyte N)
+    {
+        long fact = 1;
+
+        if (N > 20)
+        {
+            var bigFact = new BigInteger(1);
+
+            for (int i = 1; i <= N; i++)
+            {
+                bigFact *= i;
+            }
+            Console.WriteLine(bigFact);
+        }
+        else
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
+
