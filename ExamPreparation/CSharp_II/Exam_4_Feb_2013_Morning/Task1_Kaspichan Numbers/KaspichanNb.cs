6e656bea9ec0f8d314f516478a3b642dcb0a5361
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-03-04 10:46:37 +0200

    Exam 4 Feb 2013 Morning - Tasks 1 and 2

---
+++ ExamPreparation/CSharp_II/Exam_4_Feb_2013_Morning/Task1_Kaspichan Numbers/KaspichanNb.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class KaspichanNb
+{
+    static void Main()
+    {
+
+        BigInteger inputNb = new BigInteger();
+
+        inputNb = BigInteger.Parse(Console.ReadLine());
+
+
+        string[] numberValues = new string[256];
+
+
+        ushort counter = 0;
+
+        for (char i = 'A'; i <= 'Z'; i++)
+        {
+            numberValues[counter] = i.ToString();
+            counter++;
+            //Console.WriteLine(numberValues[counter]);
+        }
+
+        for (char i = ('a'); i <= ('i'); i++)
+        {
+            for (char j = 'A'; j <= ('Z'); j++)
+            {
+                numberValues[counter] = i.ToString() + j.ToString();
+                //Console.WriteLine(numberValues[counter]);
+                if (counter == 255)
+                {
+                    break;
+                } 
+                counter++;
+            }
+        }
+
+        ConvertTo256(numberValues, inputNb);
+    }
+
+    static void ConvertTo256(string[] numberValues, BigInteger inputNb)
+    {
+        StringBuilder resultStr = new StringBuilder();
+
+        BigInteger residual = 0;
+
+        do
+        {
+            residual = inputNb % (BigInteger)256;
+            inputNb /= (BigInteger)256;
+            resultStr.Insert(0,numberValues[int.Parse(residual.ToString())]);
+            
+        } 
+        while (inputNb > 0);
+
+        Console.WriteLine(resultStr.ToString());  
+    }
+
+}
+
---
+++ ExamPreparation/CSharp_II/Exam_4_Feb_2013_Morning/Task2_Greedy Dwarf/GreedyDwarf.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+
+class GreedyDwarf
+{
+    static void Main()
+    {
+        int[] valley = Console.ReadLine()
+                          .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                          .Select(int.Parse)
+                          .ToArray();
+        int M = int.Parse(Console.ReadLine());
+
+        long maxSum = long.MinValue;
+        long currentSum = 0;
+        bool isOutOfValley = false;
+        var stepsMade = new List<int>();
+
+        for (int i = 0; i < M; i++)
+        {
+            int currentPlaceInValley = 0;
+            int[] stepsInValley = Console.ReadLine()
+                          .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                          .Select(int.Parse)
+                          .ToArray();
+
+            currentSum = valley[0];    // first element aways present
+            stepsMade.Add(0);         // first element aways present
+            //int stepsCounter = 0;
+
+            while (!isOutOfValley)
+            {
+                foreach (var step in stepsInValley)
+                {
+                    currentPlaceInValley += step;
+
+                    if ((currentPlaceInValley > valley.Length - 1) || (currentPlaceInValley <= 0) || stepsMade.Exists(x => x == currentPlaceInValley))
+                    {
+                        //stepsMade.Clear();
+                        isOutOfValley = true;
+                        break;
+                    }
+
+                    currentSum += valley[currentPlaceInValley];
+
+                    stepsMade.Add(currentPlaceInValley);
+                }
+            }
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+            }
+            isOutOfValley = false;
+            stepsMade.Clear();
+        }
+        Console.WriteLine(maxSum);
+    }
+}
+
