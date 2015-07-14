046ddaa19061e21488bdc26bd18f7c5ded79d375
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-07-14 08:06:29 +0300

    Homework High-Quality Code - 4. Variables Data Expressions and Constants - All Tasks

---
+++ Homeworks/HighQualityCode/04.Variables Data Expressions and Constants/Task 1.Class Size in C/Size.cs
@@ -0,0 +1,55 @@
+﻿namespace VariablesDataExpressions
+{
+    using System;
+
+    public class Size
+    {
+        private double width;
+        private double height;
+
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get 
+            { 
+                return this.width;
+            }
+
+            set
+            {
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get 
+            {
+                return this.height;
+            }
+
+            set
+            {
+                this.height = value;
+            }
+        }
+
+        public static Size GetRotatedSize(Size size, double angleOfRotation)
+        {
+            double width = (Math.Abs(Math.Cos(angleOfRotation)) * size.Width) +
+                           (Math.Abs(Math.Sin(angleOfRotation)) * size.Height);
+
+            double height = (Math.Abs(Math.Sin(angleOfRotation)) * size.Width) +
+                            (Math.Abs(Math.Cos(angleOfRotation)) * size.Height);
+
+            Size sizeAfterRotation = new Size(width, height);
+
+            return sizeAfterRotation;
+        }
+    }
+}
---
+++ Homeworks/HighQualityCode/04.Variables Data Expressions and Constants/Task 2.MethodPrintStatistics/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Task_2.MethodPrintStatistics
+{
+    using System;
+
+    internal class Program
+    {
+        public void PrintStatistics(double[] importantNumbers)
+        {
+            double maxValue = importantNumbers[0];
+            int lenght = importantNumbers.Length;
+
+            for (int i = 0; i < lenght; i++)
+            {
+                if (importantNumbers[i] > maxValue)
+                {
+                    maxValue = importantNumbers[i];
+                }
+            }
+
+            this.PrintResult(maxValue);
+
+            double minValue = importantNumbers[0];
+
+            for (int i = 0; i < lenght; i++)
+            {
+                if (importantNumbers[i] < minValue)
+                {
+                    minValue = importantNumbers[i];
+                }
+            }
+
+            this.PrintResult(minValue);
+
+            double sumOfElements = 0;
+
+            for (int i = 0; i < lenght; i++)
+            {
+                sumOfElements += importantNumbers[i];
+            }
+
+            double averagedValue = sumOfElements / lenght;
+
+            this.PrintResult(averagedValue);
+        }
+
+        private void PrintResult(double result)
+        {
+            Console.WriteLine("The result is: {0}", result);
+        }
+    }
+}
